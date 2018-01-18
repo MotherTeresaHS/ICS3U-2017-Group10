d9a91f85854dc0d4076dab060147c8161888a36c
James  Lee <khljames@hotmail.com>
2018-01-18 14:46:46 -0500

    Added more buttons

--- instructions_scene.py
+++ instructions_scene.py
@@ -6,10 +6,6 @@
 from scene import *
 import ui
 import time 
-from objc_util import *
-
-UIScreen = ObjCClass('UIScreen')
-screen = UIScreen.mainScreen()
 
 class InstructionsScene(Scene):
     def setup(self):
@@ -18,6 +14,8 @@ class InstructionsScene(Scene):
         self.centre_of_screen = self.size/2
         self.left_button_down = False
         self.right_button_down = False
+        self.fire_button_down = False
+        self.pause_button_down = False
         
         # add background color
         self.background = SpriteNode('./assets/sprites/instructions_scene_picture.JPG',
@@ -27,7 +25,7 @@ class InstructionsScene(Scene):
 
         left_button_position = self.centre_of_screen
         left_button_position.x = self.centre_of_screen.x / 5.8
-        left_button_position.y = 100
+        left_button_position.y = self.centre_of_screen.y / 4
         self.left_button = SpriteNode('./assets/sprites/left_button.png',
                                        parent = self,
                                        position = left_button_position,
@@ -35,10 +33,27 @@ class InstructionsScene(Scene):
                                      
         right_button_position = self.centre_of_screen
         right_button_position.x = self.centre_of_screen.x * 2.6
-        right_button_position.y = 100
+        right_button_position.y = self.centre_of_screen.y 
         self.right_button = SpriteNode('./assets/sprites/right_button.png',
                                        parent = self,
                                        position = right_button_position,
+                                       alpha = 2)
+
+        fire_button_position = Vector2()
+        fire_button_position.x = self.centre_of_screen.x * 4.1
+        fire_button_position.y = self.centre_of_screen.y 
+        self.fire_button = SpriteNode('./assets/sprites/red_button.png',
+                                       parent = self,
+                                       position = fire_button_position,
+                                       alpha = 2)
+
+        pause_button_position = Vector2()
+        pause_button_position.x = self.centre_of_screen.x * 4
+        pause_button_position.y = self.centre_of_screen.y * 7.5
+        self.pause_button = SpriteNode('./assets/sprites/help.png',
+                                       parent = self,
+                                       position = pause_button_position,
+                                       scale = 0.5,
                                        alpha = 2)
         #screen.setBrightness_(0.1)
         self.explain_label = LabelNode(text = '',
@@ -46,28 +61,44 @@ class InstructionsScene(Scene):
                                       parent = self,
                                       color = 'black',
                                       position = self.size/2)
-            
+
     def update(self):
         # this method is called, hopefully, 60 times a second
         if self.left_button_down == True:
-            screen.setBrightness_(0.1)
             self.left_button.alpha = (0.5)
+            self.left_button.scale = 0.9
             self.explain_label.text = 'Moves your character to the left'
 
         if self.right_button_down == True:
-            screen.setBrightness_(0.1)
             self.right_button.alpha = (0.5)
+            self.right_button.scale = 0.9
             self.explain_label.text = 'Moves your character to the right'
+
+        if self.fire_button_down == True:
+            self.fire_button.alpha = (0.5)
+            self.fire_button.scale = 0.9
+            self.explain_label.text = 'Fires a note'
+
+        if self.pause_button_down == True:
+            self.pause_button.alpha = (0.5)
+            self.pause_button.scale = 0.4
+            self.explain_label.text = 'Pauses the game'
         pass
-    
+
     def touch_began(self, touch):
         # this method is called, when user touches the screen
-        #screen.setBrightness_(0.1)
+
         if self.left_button.frame.contains_point(touch.location):
             self.left_button_down = True
 
         if self.right_button.frame.contains_point(touch.location):
             self.right_button_down = True
+
+        if self.fire_button.frame.contains_point(touch.location):
+            self.fire_button_down = True
+
+        if self.pause_button.frame.contains_point(touch.location):
+            self.pause_button_down = True
         pass
     
     def touch_moved(self, touch):
@@ -76,11 +107,19 @@ class InstructionsScene(Scene):
     
     def touch_ended(self, touch):
         # this method is called, when user releases a finger from the screen
-        screen.setBrightness_(0.6)
         self.left_button_down = False
         self.right_button_down = False
+        self.fire_button_down = False
+        self.pause_button_down = False
         self.left_button.alpha = 2
+        self.left_button.scale = 1
         self.right_button.alpha = 2
+        self.right_button.scale = 1
+        self.fire_button.alpha = 2
+        self.fire_button.scale = 1
+        self.pause_button.alpha = 2
+        self.pause_button.scale = 0.5
+        
         self.explain_label.text = ''
         pass
     
@@ -98,3 +137,4 @@ class InstructionsScene(Scene):
         # this method is called, when user place app from background 
         # back into use. Reload anything you might need.
         pass
+    
